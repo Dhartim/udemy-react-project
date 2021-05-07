214f6ecd65c914d393a62cec73c44a9642210759
Dharti Madeka <dpmadeka@dons.usfca.edu>
2021-05-07 15:33:49 -0700

    completed till lecture 49

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PlaceItem from '../components/PlaceItem';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [{
+    id: "p1",
+    title: "Empire State building",
+    description: "One of the most sky scraper in the world",
+    imageUrl: ""
+
+}]
 
 const UserPlaces = () => {
-
+    
+    return <PlaceList items={} />
 }
 
 export default UserPlaces;
